8e2083250db620d5c6ec75b9bff74aa8d85b8a3e
Eric Hacke <eric.hacke@instacart.com>
2025-01-21 10:01:46 -0500

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,29 @@
-import sortKeysFix from 'eslint-plugin-sort-keys-fix';
+import sortKeysFix from "eslint-plugin-sort-keys-fix";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default {
-  plugins: { 'sort-keys-fix': sortKeysFix },
-  extends: ['airbnb-base', 'plugin:prettier/recommended', 'plugin:unicorn/recommended', 'prettier', 'prettier/unicorn'],
-  rules: {
-    'sort-keys-fix/sort-keys-fix': 'error',
-  },
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "plugin:unicorn/recommended",
+    "prettier",
+    "prettier/unicorn",
+), {
+    plugins: {
+        "sort-keys-fix": sortKeysFix,
+    },
+
+    rules: {
+        "sort-keys-fix/sort-keys-fix": "error",
+    },
+}];
