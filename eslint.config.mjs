e1fea07d188e7ee2e71a126a23033a0453a82857
Eric Hacke <eric.hacke@instacart.com>
2025-01-21 09:51:31 -0500

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
+import sortKeysFix from 'eslint-plugin-sort-keys-fix';
+
 export default {
-  plugins: ['sort-keys-fix'],
+  plugins: { 'sort-keys-fix': sortKeysFix },
   extends: ['airbnb-base', 'plugin:prettier/recommended', 'plugin:unicorn/recommended', 'prettier', 'prettier/unicorn'],
   rules: {
     'sort-keys-fix/sort-keys-fix': 'error',
