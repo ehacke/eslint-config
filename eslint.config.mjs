9534ce06ae0dd6a23737368f7a21d800dcfa054b
Eric Hacke <eric.hacke@instacart.com>
2025-01-21 10:02:07 -0500

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,26 @@
-import sortKeysFix from "eslint-plugin-sort-keys-fix";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import sortKeysFix from 'eslint-plugin-sort-keys-fix';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
-    "airbnb-base",
-    "plugin:prettier/recommended",
-    "plugin:unicorn/recommended",
-    "prettier",
-    "prettier/unicorn",
-), {
+export default [
+  ...compat.extends('airbnb-base', 'plugin:prettier/recommended', 'plugin:unicorn/recommended', 'prettier', 'prettier/unicorn'),
+  {
     plugins: {
-        "sort-keys-fix": sortKeysFix,
+      'sort-keys-fix': sortKeysFix,
     },
 
     rules: {
-        "sort-keys-fix/sort-keys-fix": "error",
+      'sort-keys-fix/sort-keys-fix': 'error',
     },
-}];
+  },
+];
